d382f827171b5355453533ce8cd8520d43eb8757
venkatasykam <venkatasykam@yahoo.com>
2018-06-05 13:59:49 +0530

    Update ReposForOneOrg.sh

--- Day3/Examples/ReposForOneOrg.sh
+++ Day3/Examples/ReposForOneOrg.sh
@@ -14,7 +14,7 @@ read -s Passowrd
 
 echo "Org name: ${Org}" >> OrgsAndRepos.txt
 
-curl -i -u ${Username}:${Passowrd} https://api.github.com/orgs/${Org}/repos?type=all | grep -w '"name":' | awk ' /'"name"'/ {print $1,$2} ' > ${Org}_tempRepos.txt
+curl -# -i -u ${Username}:${Passowrd} https://api.github.com/orgs/${Org}/repos?type=all | grep -w '"name":' | awk ' /'"name"'/ {print $1,$2} ' > ${Org}_tempRepos.txt
 
 awk '{ print $2 }' ${Org}_tempRepos.txt > ${Org}_tempReposList.txt
 
