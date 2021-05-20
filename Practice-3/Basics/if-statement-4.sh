77940589557649b12074df435debf4d27f8ae0d9
SVN Reddy <venkatasykam@yahoo.com>
2021-05-20 19:20:55 +0530

    Update if-statement-4.sh

--- Practice-3/Basics/if-statement-4.sh
+++ Practice-3/Basics/if-statement-4.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-X=0
-while [ -n "$X" ]
-do
-  echo -en "Enter some text (press enter to quit): "
-  read X
-  echo "You said: $X"
-done
+
+DIR="sampleDir"
+
+if [ -d "$DIR" ]
+then
+        echo "$DIR directory  exists!"
+
+		if [ -f $DIR/file.txt ]; then
+			echo "file exists: new line from nested if block" > $DIR/file.txt
+		else
+			echo "file does not exists: new line from nested else" > $DIR/file.txt
+			cat 
+		fi
+else
+        echo "$DIR directory not found!"
+        mkdir $DIR
+        echo "new line from else block" > $DIR/file.txt
+fi
