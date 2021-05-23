6f30e2eb0ec8264750c3bc1ea8e3e5965213832e
SVN Reddy <venkatasykam@yahoo.com>
2021-05-23 12:20:20 +0530

    Update forloop-4.sh

--- Practice-3/Basics/forloop-4.sh
+++ Practice-3/Basics/forloop-4.sh
@@ -4,3 +4,16 @@ for (( i=2; i <= $max; ++i ))
 do
     echo "$i"
 done
+
+#syntax for loop
+
+: <<'end_long_comment'
+
+for (( initializer; condition; incrementor/decrementor ))
+do
+	stagemets;
+	stagemets;
+	stagemets;
+done
+end_long_comment
+
